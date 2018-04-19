a6696ee380e2b9f1009a754c22e2d062f2959999
kavitha <kavitha.veluchamy@gmail.com>
2018-04-18 22:03:42 -0400

    friends api get and post, home and survey htmls are completed

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friendsArray = [
+    {
+    "name": "Ahmed",
+    "photo": "../../images/man.png",
+    "scores": ["5","1","4","4","5","1","2","5","4","1"]
+    },
+    {
+    "name": "Kathy Deming",
+    "photo": "../../images/woman.png",
+    "scores": ["4","2","5","1","3","2","2","1","3","2"]
+    }
+];
+
+module.exports = friendsArray;
---
+++ routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var path = require("path");
+var friends = require("../app/data/friends");
+
+module.exports = function(app){
+    app.get("/api/data/friends", function(req, res){
+        res.json(friends);
+    });
+
+    app.post("/api/data/friends", function(req,res){
+        var newFriend = req.body;
+        friends.push(newFriend);
+    });
+}
