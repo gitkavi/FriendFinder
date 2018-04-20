74805fbbae426d79ea93aef7df0dd3b7ca891b15
kavitha <kavitha.veluchamy@gmail.com>
2018-04-20 12:41:40 -0400

    fine tuned the conosle.logs

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,8 +1,6 @@
 var path = require("path");
 var friends = require("../app/data/friends");
 
-console.log("friends", friends)
-
 module.exports = function(app){
     app.get("/api/data/friends", function(req, res){
         res.json(friends);
